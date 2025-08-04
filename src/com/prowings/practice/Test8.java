6c4409951bb4229c45bf5dc9cafa8c84fd2ffb1a
pramodjadhav101095 <pramodjadhav101095@gmail.com>
2025-08-04 11:33:47 +0530

    new test8 class

---
+++ src/com/prowings/practice/Test8.java
@@ -0,0 +1,12 @@
+package com.prowings.practice;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Test class number 8");
+	}
+
+}
